425afad7cf7bbe32334bae73821edac71aa845b9
jupl <juan.placencia.512@gmail.com>
2014-10-13 09:37:37 -0500

    Go back to rump:server

--- gulp/server.js
+++ gulp/server.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var browserSync = require('browser-sync');
-var chalk = require('chalk');
 var extend = require('extend');
 var gulp = require('gulp');
 var path = require('path');
 var rump = require('rump');
 
-gulp.task('rump:serve', ['rump:watch'], function(callback) {
+gulp.task('rump:server', ['rump:watch'], function(callback) {
   var options = rump.configs.browserSync;
   var files = path.join(rump.configs.main.paths.destination.root,
                         rump.configs.main.globs.watch.server);
@@ -16,8 +15,4 @@ gulp.task('rump:serve', ['rump:watch'], function(callback) {
     options = extend({files: files}, options);
   }
   browserSync(options, callback);
-});
-
-gulp.task('rump:server', ['rump:serve'], function() {
-  console.log(chalk.red('rump:server is deprecated. Use rump:serve instead.'));
 });
