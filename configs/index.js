18a2d5b33b4c5bcf581ce433b2d98bc0018a56b8
jupl <juan.placencia.512@gmail.com>
2014-10-13 19:53:44 -0500

    Add port information and fix task names

--- configs/index.js
+++ configs/index.js
@@ -12,14 +12,19 @@ exports.rebuild = function() {
     }
   }, rump.configs.main.globs);
 
+  rump.configs.main.server = extend(true, {
+    port: parseInt(process.env.PORT, 10) || 3000
+  }, rump.configs.main.server);
+
   exports.browserSync = extend(true, {
     ghostMode: development,
     notify: development,
     online: false,
+    port: rump.configs.main.server.port,
     server: {
       baseDir: rump.configs.main.paths.destination.root
     }
-  }, rump.configs.main.server);
+  }, rump.configs.main.server.browserSync);
 };
 
 exports.rebuild();
--- gulp/info.js
+++ gulp/info.js
@@ -6,14 +6,15 @@ var path = require('path');
 var rump = require('rump');
 var pkg = require('../package');
 
-gulp.task('rump:info:static', function() {
+gulp.task('rump:info:server', function() {
   var destination = path.join(rump.configs.main.paths.destination.root,
                               rump.configs.main.paths.destination.static);
 
   console.log();
   console.log(chalk.magenta('--- Server', 'v' + pkg.version));
-  console.log('Static files from', chalk.green(destination), 'are served');
+  console.log('Static files from', chalk.green(destination), 'are served',
+              'on port', chalk.yellow(rump.configs.browserSync.port));
   console.log();
 });
 
-gulp.tasks['rump:info'].dep.push('rump:info:static');
+gulp.tasks['rump:info'].dep.push('rump:info:server');
